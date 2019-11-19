e46bfd99606438c77f2b2a67dbfa7e8013f937ef
Sait Cakmak <saitcakmak@outlook.com>
2019-11-18 19:42:17 -0500

    optimize via optimize_acqf

--- VaR_KG.py
+++ VaR_KG.py
@@ -12,16 +12,17 @@ from botorch.models.model import Model
 from torch.distributions import Distribution, Uniform
 from botorch import settings
 from botorch.sampling.samplers import IIDNormalSampler
-from botorch.gen import gen_candidates_scipy, gen_candidates_torch
-from botorch.acquisition.analytic import PosteriorMean
+from botorch.gen import gen_candidates_scipy
+from botorch.optim import optimize_acqf
 
 
 class InnerVaR(MCAcquisitionFunction):
     r"""
     This is the inner optimization problem of VaR-KG
     """
+
     def __init__(self, model: Model, distribution: Distribution, num_samples: int,
-                 alpha: Union[Tensor, float], c: float = 0):
+                 alpha: Union[Tensor, float], c: float = 0, fixed_samples: Optional[Tensor] = None):
         r"""
         Initialize the problem for sampling
         :param model: a constructed GP model - typically a fantasy model
@@ -29,6 +30,7 @@ class InnerVaR(MCAcquisitionFunction):
         :param num_samples: number of samples to use to calculate VaR
         :param alpha: VaR risk level alpha
         :param c: the weight of the std-dev in utility function
+        :param fixed_samples: optional, fix the samples of w for numerical stability
         """
         super().__init__(model)
         self.distribution = distribution
@@ -36,6 +38,7 @@ class InnerVaR(MCAcquisitionFunction):
         self.alpha = float(alpha)
         self.c = c
         self.num_calls = 0  # for debugging purposes
+        self.fixed_samples = fixed_samples
 
     def forward(self, X: Tensor) -> Tensor:
         r"""
@@ -45,40 +48,45 @@ class InnerVaR(MCAcquisitionFunction):
         TODO: Can we make the sampling of w work for a d dimensional random variable?
         """
         self.num_calls += 1
+        # make sure X has proper shape
+        if X.dim() < 2:
+            X.unsqueeze(0)
         with torch.enable_grad(), settings.propagate_grads(True):
             VaRs = torch.empty([X.size()[0], 1])
             # Separately calculate VaR for each entry in X
             for i in range(X.size()[0]):
                 # sample w and concatenate with x
-                w = self.distribution.rsample((self.num_samples, 1))
+                if self.fixed_samples is None:
+                    w = self.distribution.rsample((self.num_samples, 1))
+                else:
+                    w = self.fixed_samples
                 z = torch.cat((X[i].repeat(self.num_samples, 1), w), 1)
                 # sample from posterior at w
                 post = self.model.posterior(z)
-                samples = torch.squeeze(post.mean, 0)
-
+                samples = post.mean.reshape(-1, 1)
                 # We can similarly query the variance and use VaR(mu - c Sigma) as an alternative acq func.
                 if self.c != 0:
-                    samples_variance = torch.squeeze(post.variance.pow(1/2), 0)
+                    samples_variance = post.variance.pow(1 / 2).reshape(-1, 1)
                     samples = samples - self.c * samples_variance
 
                 # order samples
-                samples, index = samples.sort(-2)  # -2 for the old version
+                samples, index = samples.sort(-2)
                 # return the sample quantile
                 VaRs[i] = samples[int(self.num_samples * self.alpha)]
             # return negative so that the optimization minimizes the function
-            return -VaRs
+            # TODO: make sure this squeeze doesn't break anything
+            return -VaRs.squeeze()
 
 
 class VaRKG(MCAcquisitionFunction):
     r"""
     The VaR-KG acquisition function.
-    TODO: right now, the inner function works with multi-starts. We should make VaR-KG do the same.
     TODO: this can easily be extended to q-batch evaluation. Simply change the fantasize method to use multiple X
     """
 
     def __init__(self, model: Model, distribution: Distribution, num_samples: int, alpha: Union[Tensor, float],
                  current_best_VaR: Optional[Tensor], num_fantasies: int, dim_x: int, num_inner_restarts: int,
-                 l_bound: Union[float, Tensor], u_bound: Union[float, Tensor]):
+                 l_bound: Union[float, Tensor], u_bound: Union[float, Tensor], fix_samples: bool = False):
         r"""
         Initialize the problem for sampling
         :param model: a constructed GP model
@@ -91,13 +99,14 @@ class VaRKG(MCAcquisitionFunction):
         :param num_inner_restarts: number of starting points for inner optimization
         :param l_bound: lower bound for inner restart points
         :param u_bound: upper bound for inner restart points
+        :param fix_samples: if True, fixed samples are used for w
         """
         super().__init__(model)
         self.distribution = distribution
         self.num_samples = num_samples
         self.alpha = alpha
         if current_best_VaR is not None:
-            self.current_best_VaR = current_best_VaR
+            self.current_best_VaR = current_best_VaR.detach()
         else:
             self.current_best_VaR = Tensor([0])
         self.num_fantasies = num_fantasies
@@ -106,25 +115,49 @@ class VaRKG(MCAcquisitionFunction):
         self.l_bound = l_bound
         self.u_bound = u_bound
         self.num_calls = 0  # for debugging purposes
+        self.fix_samples = fix_samples
 
     def forward(self, X: Tensor) -> Tensor:
         r"""
         Calculate the value of VaRKG acquisition function by averaging over fantasies
-        :param X: The X: (x, w) at which VaR-KG is being evaluated
-        :return: value of VaR-KG at X (to be maximized)
+        # TODO: this can potentially be improved by getting rid of the for loops and utilizing the batch evaluations
+        :param X: The X: (x, w) at which VaR-KG is being evaluated - now allows for batch evaluations, size (n x dim)
+        :return: value of VaR-KG at X (to be maximized) - size (n)
         """
         self.num_calls += 1
+        # X.requires_grad = True  # this does not seem to do anything
+        if self.fix_samples:
+            # TODO: generalize this to mutlidimensional w
+            fixed_samples = torch.linspace(self.l_bound, self.u_bound, self.num_samples).reshape(self.num_samples, 1)
+        else:
+            fixed_samples = None
+        # make sure X has proper shape
+        if X.dim() < 2:
+            X.unsqueeze(0)
         with torch.enable_grad(), settings.propagate_grads(True):
-            inner_VaRs = torch.empty(self.num_fantasies)
-            for i in range(self.num_fantasies):
-                fantasy_model = self.model.fantasize(X, IIDNormalSampler(1))
-                inner_VaR = InnerVaR(fantasy_model, self.distribution, self.num_samples, self.alpha)
-                inner_VaRs[i] = self.optimize_inner(inner_VaR)
-            return self.current_best_VaR - inner_VaRs.mean()
+            values = torch.empty([X.size()[0], 1])
+            # separately calculate for each X
+            for i in range(X.size()[0]):
+                inner_values = torch.empty(self.num_fantasies)
+                for j in range(self.num_fantasies):
+                    fantasy_model = self.model.fantasize(X[i].unsqueeze(0), IIDNormalSampler(1))
+                    inner_VaR = InnerVaR(model=fantasy_model, distribution=self.distribution,
+                                         num_samples=self.num_samples,
+                                         alpha=self.alpha, fixed_samples=fixed_samples)
+                    # inner_values[j] = self.optimize_inner(inner_VaR)
+                    # alternative inner optimization
+                    _, val = optimize_acqf(inner_VaR, bounds=Tensor([[self.l_bound], [self.u_bound]]), q=1,
+                                           num_restarts=self.num_inner_restarts,
+                                           raw_samples=self.num_inner_restarts * 5)
+                    inner_values[j] = -val
+                print("X: ", X[i], " VaRKG: ", self.current_best_VaR - inner_values.mean())
+                values[i] = self.current_best_VaR - inner_values.mean()
+            return values.squeeze()
 
     def optimize_inner(self, inner_VaR: InnerVaR) -> Tensor:
         r"""
         Optimizes the given inner VaR function over the x component.
+        Replaced with optimize_acqf.
         :param inner_VaR: constructed InnerVaR object
         :return: result of optimization
         """
@@ -133,11 +166,8 @@ class VaRKG(MCAcquisitionFunction):
             uniform = Uniform(self.l_bound, self.u_bound)
             starting_sols = uniform.rsample((self.num_inner_restarts, self.dim_x))
             # optimize inner_VaR
-            # TODO: testing torch optimizers - we run into some issues
             candidates, values = gen_candidates_scipy(starting_sols, inner_VaR, self.l_bound, self.u_bound)
             # we maximize the negative of inner VaR and negate to get the minimum
             values = - values
             # return the best value found
             return torch.min(values)
-
-
--- varkg_test.py
+++ varkg_test.py
@@ -12,11 +12,15 @@ from botorch.gen import gen_candidates_scipy, gen_candidates_torch
 from time import time
 from typing import Union
 from botorch.optim import optimize_acqf
+from multiprocessing import Pool
 
 r"""
 Some notes for future updates:
 When updating the model with new samples, we can use ExactGP.get_fantasy_model instead of fitting from scratch.
 There is some condition_on_observations() method as well. This might be of use too.
+        #   On separate testing, condition on observations works
+        #   get_fantasy_model also works. These two give slightly different outputs. the diff is 10^-8
+        #   condition on observations actually calls  get fantasy model in the end
 """
 # TODO: KG values seem appropriate. KG optimization, however, does not make much sense.
 #       if we calculate VaRKG for multiple points with high precision, memory blows up.
@@ -26,9 +30,11 @@ There is some condition_on_observations() method as well. This might be of use t
 #       algorithms perform better. gen_candidates_scipy uses quasi-newton methods, which require somewhat stability of
 #       the gradients.
 #       We can also think about coding our own optimizers by modifying the given ones to work with these functions.
+#       Fixed samples are implemented, they increase numerical stability, however, take longer to optimize.
+#       Stick with scipy optimizers for the inner optimization.
 
 # fix the seed for testing
-# torch.manual_seed(0)
+torch.manual_seed(0)
 
 start = time()
 # sample some training data
@@ -65,16 +71,18 @@ yy = x.repeat(k, 1)
 xy = torch.cat([xx.unsqueeze(2), yy.unsqueeze(2)], 2)
 means = gp.posterior(xy).mean
 ax.scatter3D(xx.reshape(-1).numpy(), yy.reshape(-1).numpy(), means.detach().reshape(-1).numpy())
-# plt.show()
 plt.show(block=False)
 plt.pause(0.01)
+
+# reset the seed to a random value
+# torch.seed()
 
 # construct the sampling distribution of w
 dist = Uniform(0, 1)
 
 
 def KG_test(sol: Tensor, num_samples: int = 100, alpha: Union[Tensor, float] = 0.7,
-            current_best: Tensor = Tensor([0]), num_fantasies: int = 10):
+            current_best: Tensor = Tensor([0]), num_fantasies: int = 10, fix_samples=False):
     """
     this is for testing VaRKG
     :param sol: solution (1 x dim) to evaluate
@@ -82,11 +90,13 @@ def KG_test(sol: Tensor, num_samples: int = 100, alpha: Union[Tensor, float] = 0
     :param alpha: the VaR level
     :param current_best: the current best VaR value for use in VaRKG calculations
     :param num_fantasies: number of fantasy models to average over for VaRKG
+    :param fix_samples: use fix samples for w
     :return: changing
     """
     # construct the acquisition function
     var_kg = VaRKG(model=gp, distribution=dist, num_samples=num_samples, alpha=alpha, current_best_VaR=current_best,
-                   num_fantasies=num_fantasies, dim_x=1, num_inner_restarts=5, l_bound=0, u_bound=1)
+                   num_fantasies=num_fantasies, dim_x=1, num_inner_restarts=5, l_bound=0, u_bound=1,
+                   fix_samples=fix_samples)
 
     # query the value of acquisition function
     value = var_kg(sol)
@@ -94,8 +104,8 @@ def KG_test(sol: Tensor, num_samples: int = 100, alpha: Union[Tensor, float] = 0
     return value
 
 
-def KG_opt_test(start_sol: Tensor, num_samples: int = 100, alpha: Union[Tensor, float] = 0.7,
-                current_best: Tensor = Tensor([0]), num_fantasies: int = 10):
+def KG_opt_test_torch(start_sol: Tensor, num_samples: int = 100, alpha: Union[Tensor, float] = 0.7,
+                      current_best: Tensor = Tensor([0]), num_fantasies: int = 10, fix_samples=False):
     """
     this is for testing VaRKG
     :param start_sol: starting solution (1 x dim) to evaluate
@@ -103,19 +113,62 @@ def KG_opt_test(start_sol: Tensor, num_samples: int = 100, alpha: Union[Tensor, 
     :param alpha: the VaR level
     :param current_best: the current best VaR value for use in VaRKG calculations
     :param num_fantasies: number of fantasy models to average over for VaRKG
+    :param fix_samples: use fix samples for w
+    :return: changing
+    TODO: stick with gen_candidates_scipy. Torch doesn't work as well.
+    """
+    # construct the acquisition function
+    var_kg = VaRKG(model=gp, distribution=dist, num_samples=num_samples, alpha=alpha, current_best_VaR=current_best,
+                   num_fantasies=num_fantasies, dim_x=1, num_inner_restarts=5, l_bound=0, u_bound=1,
+                   fix_samples=fix_samples)
+
+    # optimize it
+    candidates, values = gen_candidates_torch(start_sol, var_kg, 0, 1)
+    print("cand:", candidates, "vals: ", values)
+    return candidates, values
+
+
+def KG_opt_test_scipy(start_sol: Tensor, num_samples: int = 100, alpha: Union[Tensor, float] = 0.7,
+                      current_best: Tensor = Tensor([0]), num_fantasies: int = 10, fix_samples=False):
+    """
+    this is for testing VaRKG
+    :param start_sol: starting solution (1 x dim) to evaluate
+    :param num_samples: number of w to samples for inner VaR calculations
+    :param alpha: the VaR level
+    :param current_best: the current best VaR value for use in VaRKG calculations
+    :param num_fantasies: number of fantasy models to average over for VaRKG
+    :param fix_samples: use fix samples for w
     :return: changing
     """
     # construct the acquisition function
     var_kg = VaRKG(model=gp, distribution=dist, num_samples=num_samples, alpha=alpha, current_best_VaR=current_best,
-                   num_fantasies=num_fantasies, dim_x=1, num_inner_restarts=5, l_bound=0, u_bound=1)
+                   num_fantasies=num_fantasies, dim_x=1, num_inner_restarts=5, l_bound=0, u_bound=1,
+                   fix_samples=fix_samples)
 
     # optimize it
-    # TODO: KG optimization doesn't really do anything.
-    #       Testing torch optimizers - we have some issue with retain-graph.
-    #       It is likely that as we take multiple derivatives with the same starting solution, we need to change
-    #       some options to enable this.
     candidates, values = gen_candidates_scipy(start_sol, var_kg, 0, 1)
-    values = - values
+    print("cand:", candidates, "vals: ", values)
+    return candidates, values
+
+
+def KG_opt_test_opt(num_samples: int = 100, alpha: Union[Tensor, float] = 0.7,
+                      current_best: Tensor = Tensor([0]), num_fantasies: int = 10, fix_samples=False):
+    """
+    this is for testing VaRKG
+    :param num_samples: number of w to samples for inner VaR calculations
+    :param alpha: the VaR level
+    :param current_best: the current best VaR value for use in VaRKG calculations
+    :param num_fantasies: number of fantasy models to average over for VaRKG
+    :param fix_samples: use fix samples for w
+    :return: changing
+    """
+    # construct the acquisition function
+    var_kg = VaRKG(model=gp, distribution=dist, num_samples=num_samples, alpha=alpha, current_best_VaR=current_best,
+                   num_fantasies=num_fantasies, dim_x=1, num_inner_restarts=5, l_bound=0, u_bound=1,
+                   fix_samples=fix_samples)
+
+    # optimize it
+    candidates, values = optimize_acqf(var_kg, bounds=Tensor([[0, 0], [1, 1]]), q=1, num_restarts=5, raw_samples=25)
     print("cand:", candidates, "vals: ", values)
     return candidates, values
 
@@ -142,11 +195,32 @@ def inner_opt_test(sols: Tensor, num_samples: int = 100, alpha: Union[Tensor, fl
     :param alpha: the VaR level
     :return: optimized points and inner VaR values (num_starting_sols x dim_x, num_starting_sols x 1)
     """
+    # TODO: fixed samples provide numerical stability here but take longer to optimize
     # construct the acquisition function
-    inner_VaR = InnerVaR(model=gp, distribution=dist, num_samples=num_samples, alpha=alpha)
+    fixed_samples = torch.linspace(0, 1, num_samples).reshape(num_samples, 1)
+    # fixed_samples = None
+    inner_VaR = InnerVaR(model=gp, distribution=dist, num_samples=num_samples, alpha=alpha, fixed_samples=fixed_samples)
     # optimize
-    # TODO: testing torch optimizers - now they also work - slower than scipy optimizers - doesnt work with VaRKG
     candidates, values = gen_candidates_scipy(sols, inner_VaR, 0, 1)
+    return candidates, -values
+
+
+def inner_opt_test2(num_samples: int = 100, alpha: Union[Tensor, float] = 0.7):
+    """
+    this is for testing the optimization of InnerVaR
+    :param sols: starting points for optimization loop (num_starting_sols x dim_x)
+    :param num_samples: number of w used to evaluate VaR
+    :param alpha: the VaR level
+    :return: optimized points and inner VaR values (num_starting_sols x dim_x, num_starting_sols x 1)
+    """
+    # TODO: fixed samples provide numerical stability here but take longer to optimize
+    # construct the acquisition function
+    fixed_samples = torch.linspace(0, 1, num_samples).reshape(num_samples, 1)
+    # fixed_samples = None
+    inner_VaR = InnerVaR(model=gp, distribution=dist, num_samples=num_samples, alpha=alpha, fixed_samples=fixed_samples)
+    # optimize
+    # TODO: this works after adding the squeeze to the return of inner VaR
+    candidates, values = optimize_acqf(inner_VaR, Tensor([[0], [1]]), q=1, num_restarts=10, raw_samples=100)
     return candidates, -values
 
 
@@ -155,17 +229,21 @@ def inner_opt_test(sols: Tensor, num_samples: int = 100, alpha: Union[Tensor, fl
 k = 40
 sols = torch.linspace(0, 1, k).view(-1, 1)
 VaRs = inner_test(sols, 10000, 0.7)
-print(VaRs)
+# print(VaRs)
 ax.scatter3D(sols.reshape(-1).numpy(), [1] * k, VaRs.detach().reshape(-1).numpy())
-current_best = min(VaRs)
+plt.show(block=False)
+plt.pause(0.01)
 
 # test for optimization of inner VaR
-k = 3
-start_sols = uniform.rsample((k, 1))
-print("start_sols: ", start_sols)
-cand, vals = inner_opt_test(start_sols, 10000, 0.7)
+k = 10
+# start_sols = uniform.rsample((k, 1))
+# print("start_sols: ", start_sols)
+# cand, vals = inner_opt_test(start_sols, 100, 0.7)
+cand, vals = inner_opt_test2()
+current_best = vals
 print("cand: ", cand, " values: ", vals)
 ax.scatter3D(cand.detach().reshape(-1).numpy(), [1]*k, vals.detach().reshape(-1).numpy(), marker='^', s=50)
+
 
 
 # calculate the value of VaRKG for a number of points
@@ -182,10 +260,21 @@ ax.scatter3D(cand.detach().reshape(-1).numpy(), [1]*k, vals.detach().reshape(-1)
 # rrr = Tensor(res)
 
 # test KG_opt
-# TODO: optimization doesn't really do much here
-# starting_sol = Tensor([0.5, 0.5])
-# cand, val = KG_opt_test(starting_sol, current_best=current_best)
-# print("cand:", cand, " val: ", val)
+# TODO: optimization doesn't really do much here - it does when num_fantasies is large enough
+#       torch seems to do something when allowed to run long enough but this whole thing requires further inspection.
+starting_sol = Tensor([0.5, 0.5])
+fix_samples = True
+# If true, increases the optimization time significantly - optimization time depends on the starting point as well
+# num_fantasies affects the optimization significantly
+# KG_opt_test_scipy(starting_sol, current_best=current_best, num_fantasies=10, fix_samples=fix_samples)
+# KG_opt_test_torch(starting_sol, current_best=current_best, num_fantasies=10, fix_samples=fix_samples)
+cand, val = KG_opt_test_opt()
+
+
+# test KG
+# sol = Tensor([[0.5, 0.5]])
+# KG_test(sol, current_best=current_best)
+
 
 opt_complete = time()
 print("fit: ", fit_complete - start, " opt: ", opt_complete - fit_complete)
