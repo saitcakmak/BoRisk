ce85188fbefc6b4430153178df56b80c742beff7
Sait Cakmak <saitcakmak@outlook.com>
2020-01-14 17:01:39 -0500

    mini batch size made adjustable

--- debug/optimization_test_plotter.py
+++ debug/optimization_test_plotter.py
@@ -1,12 +1,29 @@
 import torch
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 
 num_fantasies = 100
-num_restarts = 100
-raw_multiplier = 10
-repetitions = 100
+num_restarts = 400
+raw_multiplier = 25
+repetitions = 25
+dim = 2
 file = 'debug_out/debug_%d_%d_%d_%d.pt' % (num_fantasies, num_restarts, raw_multiplier, repetitions)
 
 data = torch.load(file)
 solutions = data['solutions']
+values = torch.tensor(data['kg_values']).numpy()
+
+temp = torch.empty((repetitions, dim))
+for i in range(repetitions):
+    temp[i] = solutions[i]
+solutions = temp.numpy()
+
+plt.figure(1)
+plt.scatter(solutions[:, 0], solutions[:, 1])
+plt.title('Solutions')
+
+plt.figure(2)
+plt.hist(values)
+plt.title('VaRKG values')
+plt.show()
+
