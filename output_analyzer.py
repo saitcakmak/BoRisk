f4ef638d99adaae83174fe44c1e3bca3d1b576c2
Sait Cakmak <saitcakmak@outlook.com>
2020-02-02 10:55:35 -0500

    stash

--- output_analyzer.py
+++ output_analyzer.py
@@ -10,14 +10,16 @@ from value_plotter import generate_values
 
 directory = 'loop_output/'
 # prefix = 'cluster_'
-prefix = ''
-problem_name = 'newsvendor'
-dim_w = 2
+prefix = 'comp_'
+problem_name = 'ackley'
+dim_w = 1
 iterations = 50
-k = 1000  # number of w to draw to evaluate the true values, linspace if dim_w == 1, rand otherwise
-alpha = 0.9  # risk level
+num_w = 6  # number of w to draw to evaluate the true values, linspace if dim_w == 1, rand otherwise
+num_x = 100000  # number of x used to evaluate the true values
+alpha = 0.66  # risk level
 eval_seed = 0  # seed used during final evaluations for both function eval and w generation if random
 CVaR = False
+# TODO: ackley output doesn't make sense
 
 # this is the powell seed list. Comment out and make others if needed.
 # seed = [123, 127, 1599, 18990, 2355, 234556, 9876, 7565, 45363, 243456]
@@ -25,12 +27,12 @@ CVaR = False
 # seed = [3452, 44331, 34535, 7855, 9374, 38275]
 # seed = [34578, 7563, 59274, 47238, 1946, 37521]
 # newsvendor CVaR 0.9
-seed = [23856, 83742, 75624, 34755, 38523, 57633, 73485, 12654, 93832, 43566]
-
+# seed = [23856, 83742, 75624, 34755, 38523, 57633, 73485, 12654, 93832, 43566]
+seed = [66814]
 file_name = []
 for i in range(len(seed)):
     file_name.append('run' + str(i+1))
-suffix = '_cvar_a0.9_random'
+suffix = '_a0.66q=6'
 
 file_list = []
 # if you just want to play around with a single output, use this. - still need prob name, dim_w and iterations
@@ -66,30 +68,20 @@ for j in range(len(seed)):
     for i in range(iterations):
         best_solutions[i] = data[i]['current_best_sol']
 
-    print(best_solutions)
-    full_train = data[iterations-1]['train_X']
-    print(full_train)
-    # plt.figure(3)
-    # plt.scatter(best_solutions.numpy()[:, 0], best_solutions.numpy()[:, 1])
-    # plt.figure(4)
-    # plt.scatter(full_train.numpy()[:, 0], full_train.numpy()[:, 1])
-    # plt.figure(5)
-    # plt.scatter(full_train.numpy()[:, 2], full_train.numpy()[:, 3])
-
     if problem_name == 'sinequad':
         full_solutions = torch.cat((best_solutions, torch.ones((iterations, 1)) * alpha), dim=-1)
         true_values = function.evaluate_true(full_solutions)
     else:
         true_values = torch.empty((iterations, 1))
         if dim_w == 1:
-            w = torch.linspace(0, 1, k).reshape(k, 1)
+            w = torch.linspace(0, 1, num_w).reshape(num_w, 1)
         else:
             old_state = torch.random.get_rng_state()
             torch.random.manual_seed(eval_seed)
-            w = torch.rand((k, 2))
+            w = torch.rand((num_w, 2))
             torch.random.set_rng_state(old_state)
         for i in range(iterations):
-            sol = best_solutions[i].reshape(1, -1).repeat(k, 1)
+            sol = best_solutions[i].reshape(1, -1).repeat(num_w, 1)
 
             full_sol = torch.cat((sol, w), dim=-1)
             try:
@@ -98,10 +90,10 @@ for j in range(len(seed)):
                 values = function(full_sol, seed=eval_seed)
             values, index = torch.sort(values, dim=-2)
             if CVaR:
-                true_values[i] = torch.mean(values[int(k * alpha):])
+                true_values[i] = torch.mean(values[int(num_w * alpha):])
             else:
-                true_values[i] = values[int(k * alpha)]
-    _, true_optimal = generate_values(10000, k, plug_in_w=w, CVaR=CVaR, function=function, dim_x=dim_x, dim_w=dim_w)
+                true_values[i] = values[int(num_w * alpha)]
+    _, true_optimal = generate_values(num_x, num_w, plug_in_w=w, CVaR=CVaR, function=function, dim_x=dim_x, dim_w=dim_w)
     true_optimal = torch.min(true_optimal)
 
     value_diff_list[j] = true_values - true_optimal
