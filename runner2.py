8c176c9c04330fae19d35b4051eff3ea8ad7e1c9
saitcakmak <saitcakmak@outlook.com>
2020-03-09 18:23:11 -0400

    e

--- runner2.py
+++ runner2.py
@@ -8,13 +8,13 @@ import torch
 function_name = input("function name: ")
 num_samples = 10
 num_fantasies = 50
-key_list = ['varkg_s00', 'varkg_random_s00']
+key_list = ['varkg_s00', 'kgcp_s00', 'kgcp_random_s00', 'varkg_random_s00']
 #            'varkg_s01', 'kgcp_s01', 'kgcp_random_s01', 'varkg_random_s01',
 #            'varkg_s10', 'kgcp_s10', 'kgcp_random_s10', 'varkg_random_s10',
 #            'varkg_s40', 'kgcp_s40', 'kgcp_random_s40', 'varkg_random_s40']
-output_file = "%s_%s" % (function_name, "red_dim")
+output_file = "%s_%s" % (function_name, "exp")
 torch.manual_seed(0)  # to ensure the produced seed are same!
-seed_list = torch.randint(10000, (1,))
+seed_list = torch.randint(10000, (3,))
 dim_w = 1
 iterations = 50
 num_restarts = 40
