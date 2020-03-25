8358223fc56b10fee132d8488829f600e2c1ba29
Ubuntu <ubuntu@ip-172-31-27-92.us-east-2.compute.internal>
2020-03-25 12:29:34 +0000

    e

--- aws_main.sh
+++ aws_main.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # make sure you modified the runner file accordingly
+for i in {1..10}
+do
 python -W ignore runner.py
+done
 
 # make sure to enter the instance id here
-aws ec2 stop-instances --instance-ids i-00f24de3ed5809e79
+aws ec2 stop-instances --instance-ids i-0b18e2e36984210d3
