21eb619c02d0e85dbde4d18157923bbbd9f4c7fe
Sait Cakmak <saitcakmak@outlook.com>
2020-05-23 10:40:18 -0400

    exp

--- covid-runner-other-q1.py
+++ covid-runner-other-q1.py
@@ -29,9 +29,9 @@ bm_alg_list = [ExpectedImprovement,
                UpperConfidenceBound,
                ]
 q_base = 10  # q for VaRKG. For others, it is q_base / num_samples
-iterations = 100
+iterations = 30
 
-seed_list = range(1, 11)
+seed_list = range(1, 31)
 
 output_file = "%s_%s" % (function_name, "cvar")
 torch.manual_seed(0)  # to ensure the produced seed are same!
