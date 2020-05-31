ea85ad1caf88fb0b6c6a432bab88d7b45feb3257
Sait Cakmak <saitcakmak@outlook.com>
2020-05-30 22:12:42 -0400

    random exp

--- aws_main.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# make sure you modified the runner file accordingly
-for i in {1..10}
-do
-python -W ignore runner.py
-done
-
-# make sure to enter the instance id here
-aws ec2 stop-instances --instance-ids i-04efe05390440073f
--- helper_fns/exp_out_read.py
+++ helper_fns/exp_out_read.py
@@ -6,18 +6,18 @@ import torch
 from time import time
 
 
-directory = '../exp_output/'
-function_name = 'portfolio_surrogate'
-output_key = 'tts_varkg_q=1'
-suffix = '_var_%s_' % output_key
+directory = '../exp_output/covid_benchmarks/'
+function_name = 'covid'
+output_key = 'classical_random'
+suffix = '_cvar_%s_' % output_key
 # seed_list = [6044, 8239, 4933, 3760, 8963]
-seed_list = range(1, 7)
+seed_list = range(1, 31)
 q = 1  # only used in the next line
-suffix2 = '_a=0.8_cont%s.pt' % ('_q=%d' % q if output_key in ['random', 'tts_kgcp'] and q > 1 else '')
+suffix2 = '_a=0.9_weights%s.pt' % ('_q=%d' % q if output_key in ['random', 'tts_kgcp'] and q > 1 else '')
 
-output_file = '../batch_output/plot_%s_var' % function_name
+output_file = '../batch_output/plot_%s_cvar' % function_name
 
-iterations = 15
+iterations = 30
 
 
 def read_bests(seed):
