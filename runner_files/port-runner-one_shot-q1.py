e01c1c09712266ef7c557d2aa42534c1fffa0ffd
saitcakmak <saitcakmak@outlook.com>
2020-09-24 10:39:19 -0400

    experiments

--- runner_files/port-runner-one_shot-q1.py
+++ runner_files/port-runner-one_shot-q1.py
@@ -8,45 +8,46 @@ from BoRisk.test_functions import function_picker
 
 # Modify this and make sure it does what you want!
 
-function_name = "braninwilliams"
-num_samples = 12
-num_fantasies = 4  # default 50
-key_list = ["one_shot"]
+function_name = "portfolio_surrogate"
+num_samples = 40  # this is 40 for rhoKG / apx and 10 for benchmarks
+num_fantasies = 10  # default 50
+key_list = ["one_shot_q=1"]
 # this should be a list of bm algorithms corresponding to the keys. None if rhoKG
 bm_alg_list = [None]
 q_base = 1  # q for rhoKG. For others, it is q_base / num_samples
-iterations = 40
+iterations = 83
 
 import sys
 
 seed_list = [int(sys.argv[1])]
-# seed_list = range(1, 4)
+# seed_list = range(1, 11)
 
-output_file = "%s_%s" % (function_name, "var_10fant_6start")
+output_file = "%s_%s" % (function_name, "var")
 torch.manual_seed(0)  # to ensure the produced seed are same!
 kwargs = dict()
 dim_w = 2
-kwargs["noise_std"] = 10
+kwargs["noise_std"] = 0.1
+kwargs[
+    "negate"
+] = True  # True if the function is written for maximization, e.g. portfolio
 function = function_picker(function_name)
-if dim_w > 1:
-    w_samples = None
-    w_samples = function.w_samples
-    if w_samples is None:
-        raise ValueError("Specify w_samples!")
-else:
-    w_samples = None
+kwargs["fix_samples"] = True
+w_samples = function.w_samples
 weights = function.weights
 kwargs["weights"] = weights
 dim_x = function.dim - dim_w
-num_restarts = 10 * function.dim
-raw_multiplier = 25  # default 50
+num_restarts = 20 * function.dim
+raw_multiplier = 100  # default 50
 
 kwargs["num_inner_restarts"] = 5 * dim_x
 kwargs["CVaR"] = False
 kwargs["expectation"] = False
-kwargs["alpha"] = 0.7
-kwargs["disc"] = True
-num_x_samples = 6
+kwargs["one_shot"] = True
+kwargs["alpha"] = 0.8
+kwargs["disc"] = False
+# kwargs["low_fantasies"] = 4
+num_x_samples = 8
+num_init_w = 10
 
 output_dict = dict()
 
@@ -57,19 +58,20 @@ for i, key in enumerate(key_list):
         seed = int(seed)
         print("starting key %s seed %d" % (key, seed))
         filename = output_file + "_" + key + "_" + str(seed)
-        random = "random" in key
-        apx = "apx" in key
-        if "tts" in key:
-            tts_frequency = 10
-        else:
-            tts_frequency = 1
         if num_x_samples:
             old_state = torch.random.get_rng_state()
             torch.manual_seed(seed)
             x_samples = torch.rand(num_x_samples, dim_x)
+            init_w_samples = torch.rand(num_x_samples, num_init_w, dim_w)
+            kwargs["x_samples"] = x_samples
+            kwargs["init_w_samples"] = init_w_samples
+            kwargs["init_samples"] = torch.cat(
+                (x_samples.unsqueeze(-2).repeat(1, num_init_w, 1), init_w_samples),
+                dim=-1,
+            )
             torch.random.set_rng_state(old_state)
         else:
-            x_samples = None
+            kwargs["x_samples"] = None
         if bm_alg_list[i] is None:
             q = q_base
         else:
@@ -83,12 +85,8 @@ for i, key in enumerate(key_list):
             num_samples=num_samples,
             num_fantasies=num_fantasies,
             num_restarts=num_restarts,
-            x_samples=x_samples,
             raw_multiplier=raw_multiplier,
             q=q,
-            apx=apx,
-            random_sampling=random,
-            tts_frequency=tts_frequency,
             benchmark_alg=bm_alg_list[i],
             w_samples=w_samples,
             **kwargs
--- runner_files/port-runner-one_shot2-q1.py
+++ runner_files/port-runner-one_shot2-q1.py
@@ -8,43 +8,46 @@ from BoRisk.test_functions import function_picker
 
 # Modify this and make sure it does what you want!
 
-function_name = "marzat"
-num_samples = 10  # this is 40 for rhoKG / apx and 8 for benchmarks
-num_fantasies = 4  # default 50
-key_list = ["one_shot"]
+function_name = "portfolio_surrogate"
+num_samples = 40  # this is 40 for rhoKG / apx and 10 for benchmarks
+num_fantasies = 10  # default 50
+key_list = ["one_shot_q=1"]
 # this should be a list of bm algorithms corresponding to the keys. None if rhoKG
 bm_alg_list = [None]
 q_base = 1  # q for rhoKG. For others, it is q_base / num_samples
-iterations = 50
+iterations = 83
 
 import sys
 
 seed_list = [int(sys.argv[1])]
-# seed_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# seed_list = range(1, 11)
 
-output_file = "%s_%s" % (function_name, "cvar_10fant")
+output_file = "%s_%s" % (function_name, "var")
 torch.manual_seed(0)  # to ensure the produced seed are same!
 kwargs = dict()
-dim_w = 3
-kwargs["noise_std"] = 1
-function = function_picker(function_name)
+dim_w = 2
+kwargs["noise_std"] = 0.1
 kwargs[
-    "fix_sampless"
-] = True  # This should be true. We will just pass None for w_samples to get random samples
+    "negate"
+] = True  # True if the function is written for maximization, e.g. portfolio
+function = function_picker(function_name)
+kwargs["fix_samples"] = True
 w_samples = function.w_samples
 weights = function.weights
 kwargs["weights"] = weights
 dim_x = function.dim - dim_w
-num_restarts = 10 * function.dim
-raw_multiplier = 25  # default 50
+num_restarts = 50 * function.dim
+raw_multiplier = 100  # default 50
 
 kwargs["num_inner_restarts"] = 5 * dim_x
-kwargs["CVaR"] = True
+kwargs["CVaR"] = False
 kwargs["expectation"] = False
-kwargs["alpha"] = 0.75
+kwargs["one_shot"] = True
+kwargs["alpha"] = 0.8
 kwargs["disc"] = False
-num_x_samples = 10
-num_init_w = 8
+# kwargs["low_fantasies"] = 4
+num_x_samples = 8
+num_init_w = 10
 
 output_dict = dict()
 
@@ -55,12 +58,6 @@ for i, key in enumerate(key_list):
         seed = int(seed)
         print("starting key %s seed %d" % (key, seed))
         filename = output_file + "_" + key + "_" + str(seed)
-        random = "random" in key
-        apx = "apx" in key
-        if "tts" in key:
-            tts_frequency = 10
-        else:
-            tts_frequency = 1
         if num_x_samples:
             old_state = torch.random.get_rng_state()
             torch.manual_seed(seed)
@@ -90,9 +87,6 @@ for i, key in enumerate(key_list):
             num_restarts=num_restarts,
             raw_multiplier=raw_multiplier,
             q=q,
-            apx=apx,
-            random_sampling=random,
-            tts_frequency=tts_frequency,
             benchmark_alg=bm_alg_list[i],
             w_samples=w_samples,
             **kwargs
